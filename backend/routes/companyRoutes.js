ef58f76c6f65c5003de101fc9c44100dacc87246
yogeshkar65 <yogeshkar65@gmail.com>
2026-02-04 13:27:09 +0530

    feat(backend): implement company management routes

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { addCompany, getCompanies } = require("../controllers/companyController");
+const { addCompany, getCompanies, deleteCompany } = require("../controllers/companyController");
 
 router.post("/", addCompany);
 router.get("/", getCompanies);
+router.delete("/:id", deleteCompany);
 
 module.exports = router;
