6ba4ad807757e9226b9608c046b3d3268d0b6911
yogeshkar65 <yogeshkar65@gmail.com>
2026-02-04 12:32:00 +0530

    chore: update backend server configuration and routing

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,7 @@ connectDB();
 app.use("/api/companies", require("./routes/companyRoutes"));
 app.use("/api/drives", require("./routes/driveRoutes"));
 app.use("/api/students", require("./routes/studentRoutes"));
-app.use("/api/students", require("./routes/studentRoutes"));
-
+app.use("/api/dashboard", require("./routes/dashboardRoutes"));
 
 app.get("/", (req, res) => {
   res.send("Placement Management Backend Running");
